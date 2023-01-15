e1fd8d864c66bd87bc357726d0671ed31ae2d93b
Marek Suchánek <marek.suchanek@gmail.com>
2023-01-15 14:56:24 +0100

    feat(docworker): Allow more levels of ToC using Pandoc filter

--- packages/dsw-document-worker/addons/pandoc-docx-pagebreak-py/docx_pagebreak.py
+++ packages/dsw-document-worker/addons/pandoc-docx-pagebreak-py/docx_pagebreak.py
@@ -7,41 +7,58 @@ Only for docx output
 
 - https://github.com/UoA-eResearch/pandoc-docx-pagebreak-py
 """
-
 import panflute as pf
 
 
 class DocxPagebreak(object):
-    pagebreak = pf.RawBlock("<w:p><w:r><w:br w:type=\"page\" /></w:r></w:p>", format="openxml")
-    toc = pf.RawBlock(r"""
-<w:sdt>
-    <w:sdtContent xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main">
-        <w:p>
-            <w:r>
-                <w:fldChar w:fldCharType="begin" w:dirty="true" />
-                <w:instrText xml:space="preserve">TOC \o "1-3" \h \z \u</w:instrText>
-                <w:fldChar w:fldCharType="separate" />
-                <w:fldChar w:fldCharType="end" />
-            </w:r>
-        </w:p>
-    </w:sdtContent>
-</w:sdt>
-""", format="openxml")
+
+    @staticmethod
+    def _make_pagebreak():
+        return pf.RawBlock('<w:p><w:r><w:br w:type="page" /></w:r></w:p>', format='openxml')
+
+    @staticmethod
+    def _make_toc(instr: str):
+        toc_lines = [
+            r'<w:sdt>',
+            r'<w:sdtContent xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main">',
+            r'<w:p><w:r>',
+            r'<w:fldChar w:fldCharType="begin" w:dirty="true" />',
+        ]
+
+        if instr == r"\toc1":
+            toc_lines.append(r'<w:instrText xml:space="preserve">TOC \o "1-1" \h \z \u</w:instrText>')
+        elif instr == r"\toc2":
+            toc_lines.append(r'<w:instrText xml:space="preserve">TOC \o "1-2" \h \z \u</w:instrText>')
+        elif instr == r"\toc4":
+            toc_lines.append(r'<w:instrText xml:space="preserve">TOC \o "1-4" \h \z \u</w:instrText>')
+        elif instr == r"\toc5":
+            toc_lines.append(r'<w:instrText xml:space="preserve">TOC \o "1-5" \h \z \u</w:instrText>')
+        elif instr == r"\toc6":
+            toc_lines.append(r'<w:instrText xml:space="preserve">TOC \o "1-6" \h \z \u</w:instrText>')
+        else:
+            toc_lines.append(r'<w:instrText xml:space="preserve">TOC \o "1-3" \h \z \u</w:instrText>')
+
+        toc_lines.append(r'<w:fldChar w:fldCharType="separate" />')
+        toc_lines.append(r'<w:fldChar w:fldCharType="end" />')
+        toc_lines.append(r'</w:r></w:p>')
+        toc_lines.append(r'</w:sdtContent>')
+        toc_lines.append(r'</w:sdt>')
+        return pf.RawBlock('\n'.join(toc_lines), format='openxml')
 
     def action(self, elem, doc):
-        if doc.format != "docx":
+        if doc.format != 'docx':
             return elem
         if isinstance(elem, (pf.Para, pf.Plain)):
             for child in elem.content:
-                if isinstance(child, pf.Str) and child.text == r"\newpage":
-                    elem = self.pagebreak
-                elif isinstance(child, pf.Str) and child.text == r"\toc":
-                    elem = self.toc
+                if isinstance(child, pf.Str) and child.text == r'\newpage':
+                    elem = self._make_pagebreak()
+                elif isinstance(child, pf.Str) and child.text.startswith(r'\toc'):
+                    elem = self._make_toc(child.text)
         if isinstance(elem, pf.RawBlock):
-            if elem.text == r"\newpage":
-                elem = self.pagebreak
-            elif elem.text == r"\toc":
-                elem = self.toc
+            if elem.text == r'\newpage':
+                elem = self._make_pagebreak()
+            elif elem.text.startswith(r'\toc'):
+                elem = self._make_toc(elem.text)
         return elem
 
 
@@ -50,5 +67,5 @@ def main(doc=None):
     return pf.run_filter(dp.action, doc=doc)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
