f8df1c62a19010e65665d9a7640fa7f15096fdb4
Marek Suchánek <marek.suchanek@gmail.com>
2026-03-25 12:48:36 +0000

    chore: Use dynamic version info from pyproject.toml

--- packages/dsw-mailer/dsw/mailer/consts.py
+++ packages/dsw-mailer/dsw/mailer/consts.py
@@ -1,3 +1,6 @@
+from importlib.metadata import PackageNotFoundError, version
+
+
 COMPONENT_NAME = 'Mailer'
 CMD_CHANNEL = 'mailer'
 CMD_COMPONENT = 'mailer'
@@ -5,7 +8,13 @@ CMD_FUNCTION = 'sendMail'
 DEFAULT_ENCODING = 'utf-8'
 NULL_UUID = '00000000-0000-0000-0000-000000000000'
 PROG_NAME = 'dsw-mailer'
-VERSION = '4.28.3'
+PACKAGE_NAME = 'dsw-mailer'
+
+try:
+    __version__ = version(PACKAGE_NAME)
+except PackageNotFoundError:
+    __version__ = '0.0.0'
+VERSION = __version__
 
 VAR_APP_CONFIG_PATH = 'APPLICATION_CONFIG_PATH'
 VAR_WORKDIR_PATH = 'WORKDIR_PATH'
