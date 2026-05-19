010c57b17d5812610fa77deb786a763cb7ba2d08
Marek Suchánek <marek.suchanek@gmail.com>
2026-05-19 16:37:02 +0200

    fix(mailer): Fix potential wrong style in emails

--- packages/dsw-mailer/dsw/mailer/model.py
+++ packages/dsw-mailer/dsw/mailer/model.py
@@ -73,8 +73,6 @@ class Color:
 
 
 class StyleConfig:
-    _DEFAULT = None
-
     def __init__(self, logo_url: str | None, primary_color: str,
                  illustrations_color: str):
         self.logo_url = logo_url
@@ -98,13 +96,11 @@ class StyleConfig:
 
     @classmethod
     def default(cls):
-        if cls._DEFAULT is None:
-            cls._DEFAULT = StyleConfig(
-                logo_url=None,
-                primary_color=Color.DEFAULT_PRIMARY_HEX,
-                illustrations_color=Color.DEFAULT_ILLUSTRATIONS_HEX,
-            )
-        return cls._DEFAULT
+        return StyleConfig(
+            logo_url=None,
+            primary_color=Color.DEFAULT_PRIMARY_HEX,
+            illustrations_color=Color.DEFAULT_ILLUSTRATIONS_HEX,
+        )
 
 
 class TemplateDescriptorPart:
